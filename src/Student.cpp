642e1dc3d5a783b108d2f428be50a50c4de814ca
jcribeiro <j.alberto.cribeiro@gmail.com>
2022-10-19 15:23:40 +0100

    Created Student class, and altered CMakeLists file accordingly

---
+++ src/Student.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Joao Ribeiro on 19/10/2022.
+//
+
+#include "Student.h"
+
+using namespace std;
+
+Student::Student(string n, int id, list<ClassUC> c): name(n), id(id), classes(c) {}
+
+Student::Student(const Student &s) {
+    this->name = s.name;
+    this->id = s.id;
+    this->classes = s.classes;
+}
+
+string Student::getName() { return this->name; }
+
+void Student::setName(string newName) { this->name = newName; }
+
+int Student::getId() { return this->id; }
+
+void Student::setId(int newId) { this->id = newId; }
+
+list<ClassUC> Student::getClasses() { return this->classes; }
---
+++ src/Student.h
@@ -0,0 +1,31 @@
+//
+// Created by Joao Ribeiro on 19/10/2022.
+//
+
+#ifndef PROJ1_STUDENT_H
+#define PROJ1_STUDENT_H
+
+#include <string>
+#include <list>
+
+#include "ClassUC.h"
+
+using namespace std;
+
+class Student {
+    string name;
+    int id;
+    list<ClassUC> classes;
+
+public:
+    Student(string n, int id, list<ClassUC> c);
+    Student(const Student& s);
+    string getName();
+    void setName(string newName);
+    int getId();
+    void setId(int newId);
+    list<ClassUC> getClasses();
+};
+
+
+#endif //PROJ1_STUDENT_H
