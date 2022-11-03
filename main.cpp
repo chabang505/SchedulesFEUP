b70c11be4ed0d6e3eb60861775e7f8b14278db04
jcribeiro <j.alberto.cribeiro@gmail.com>
2022-11-03 21:55:25 +0000

    Slight change to main to test features

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 // Created by Joao Ribeiro on 19/10/2022.
 //
 
+#include <iostream>
+
 #include "src/ScheduleManager.h"
 #include "src/Menu.h"
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
     ScheduleManager manager = ScheduleManager();
-    manager.createClassStudents("students_classes.csv");
+    manager.generateYears();
+    manager.readClassesPerUC("classes_per_uc.csv");
+    manager.readStudentsFile("students_classes.csv");
 }
 
